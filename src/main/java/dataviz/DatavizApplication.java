209c899b8ba699a44036ef84a3397d13b4a20c72
dapeng <yebigroc@gmail.com>
2015-09-25 18:03:26 +0800

    initial commit with empty but running application

---
+++ src/main/java/dataviz/DatavizApplication.java
@@ -0,0 +1,12 @@
+package dataviz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatavizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatavizApplication.class, args);
+    }
+}
