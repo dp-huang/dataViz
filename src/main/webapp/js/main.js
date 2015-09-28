cd4c981b349af5712e288c540e4a465dbd32609c
dapeng <yebigroc@gmail.com>
2015-09-28 22:42:25 +0800

    1) support multiple dimensions 2) setInterval to automatically refresh dashboard 3) show XY axis

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -32,17 +32,20 @@ DataViz.PageEvent.prototype = {
 			me._hidePickerDropdown();
 		});
 		//register event on canvas
-
 		$('#metricsCanvas').on('mousemove', function(e) {
 			var pos = me._getMousePos(this, e);
 			if (me._renderer) {
 				me._renderer.pick(pos);
 			}
-		})
+		});
 	},
 
 	_initialRequest: function() {
 		this._refreshDashboard(300);
+		var me = this;
+		setInterval(function(e) {
+			me._refreshDashboard(me._duration);
+		}, 5000);
 	},
 
 	_getMousePos: function (canvas, evt) {
@@ -63,6 +66,7 @@ DataViz.PageEvent.prototype = {
 		return $('.timer-picker .dropdown').css('display') == 'block';
 	},
 	_refreshDashboard: function(duration) {
+		this._duration = duration;
 		var me = this;
 		var url = DataViz.Connection.buildUrl(duration);
 		DataViz.Connection.requestGet(url).then(function(e) {
@@ -88,11 +92,22 @@ DataViz.DataRenderer = function() {
 	this._width = undefined;
 	this._height = undefined;
 	this._screenPoints = [];
+	this._colors = [
+		{ r: 126, g: 178, b: 109 },
+		{ r: 234, g: 184, b: 57 },
+		{ r: 110, g: 208, b: 224 },
+		{ r: 239, g: 132, b: 60 },
+		{ r: 226, g: 77, b: 66 },
+		{ r: 31, g: 120, b: 193 },
+		{ r: 186, g: 67, b: 169}
+	];
+	this._lineColors = {};
+	this._screenPoints = {};
 }
 
 DataViz.DataRenderer.prototype = {
 
-	_process: function(data, duration) {
+	_process: function(data) {
 		var maxValue = 0;
 		for (var key in data) {
 			var items = data[key];
@@ -102,18 +117,36 @@ DataViz.DataRenderer.prototype = {
 				}
 			});
 		}
+		this._maxValue = maxValue;
 		this._verticalScale =  this._height / maxValue;
-		this._horizontalScale = this._width / duration;
+		this._horizontalScale = this._width / this._duration;
+	},
+
+	_createRandomColor: function() {
+		return {
+			r: Math.floor(Math.random() * 255),
+			g: Math.floor(Math.random() * 255),
+			b: Math.floor(Math.random() * 255)
+		}
+	},
+
+	_convertColor: function(rgb) {
+		return 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')';
+	},
+
+	_convertAlphaColor: function(rgb, alpha) {
+		return 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ',' + alpha + ')';
 	},
 	
-	_drawDimension: function(items, duration) {
+	_drawDimension: function(key, items) {
+		var color = this._lineColors[key];
 		var ctx = this._context;
 		ctx.beginPath();
-		ctx.strokeStyle = 'red';
+		ctx.strokeStyle = this._convertColor(color);
 		ctx.lineWidth = 1;
 		var verticalScale = this._verticalScale * 0.8;
 		var screenPoints = [];
-		for (var i = 0; i < duration; i++) {
+		for (var i = 0; i < this._duration; i++) {
 			var xPos = i * this._horizontalScale;
 			var yPos = this._height - items[i].value * verticalScale;
 			if (i == 0) {
@@ -124,13 +157,68 @@ DataViz.DataRenderer.prototype = {
 			screenPoints.push({'x': xPos, 'y': yPos});
 		}
 		ctx.stroke();
-		this._screenPoints = screenPoints;
+		this._screenPoints[key] = screenPoints;
 	},
 
-	_drawDimensions: function(data, duration) {
+	_drawDimensions: function(data) {
+		var index = 0;
 		for (var key in data) {
-			this._drawDimension(data[key], duration);
+			if (index < this._colors.length) {
+				this._lineColors[key] = this._colors[index];
+			} else {
+				this._lineColors[key] = this._createRandomColor();
+			}
+			this._drawDimension(key, data[key], this._duration);
+			index++;
 		}
+	},
+
+	_showAxis: function() {
+		var date = new Date();
+		var hour = date.getHours();
+		var min = date.getMinutes();
+		var seconds = date.getSeconds();
+		var durationEach = Math.floor(this._duration / 300);
+
+		var ctx = this._context;
+		ctx.fillStyle = '#fff';
+		ctx.font="11px Verdana";
+		var endPadding = this._horizontalScale * seconds;
+		//x axis
+		for (var i = 0; i < 5; i++) {
+			var endPosX = this._width - endPadding - this._horizontalScale * i  * 60 * durationEach;
+			var min1 = min - i * durationEach;
+			while (min1 < 0) {
+				min1 += 60;
+				min += 60;
+				hour--;
+				if (hour < 0) {
+					hour = 23;
+				}
+			}
+			var displayHour = hour;
+			if (hour < 10) {
+				displayHour = '0' + hour;
+			}
+			var displayMin = min1;
+			if (min1 < 10) {
+				displayMin = '0' + min1;
+			}
+			var text = displayHour + ':' + displayMin;
+			ctx.fillText(text, endPosX - 20, this._height + 20);
+		}
+		//y axis
+		var valueEach = Math.floor(this._maxValue / 5);
+
+		for (var i = 0; i < 5; i++) {
+			var startY = this._height - this._verticalScale * valueEach * i;
+			var text = valueEach * i;
+			ctx.fillText(text, 0, startY);
+		}
+
+		ctx.fillStyle = '#000';
+
+
 	},
 
 	_binarySearch: function(find, points, low, high) {
@@ -153,14 +241,15 @@ DataViz.DataRenderer.prototype = {
 		return -1;
 	},
 
-	_getPickPoint: function(pos) {
-		var screenPointLength = this._screenPoints.length;
-		var pickXIndex = this._binarySearch(pos.x, this._screenPoints, 0, screenPointLength - 1);
+	_getSingleLinePickPoint: function(pos, screenPoints) {
+		var screenPointLength = screenPoints.length;
+		var pickXIndex = this._binarySearch(pos.x, screenPoints, 0, screenPointLength - 1);
+		var defaultReturn = {index: -1};
 		if (pickXIndex == -1) {
-			return -1;
+			return defaultReturn;
 		}
-		if (pickXIndex == 0 || pickXIndex == this._screenPoints.length - 1) {
-			return -1;
+		if (pickXIndex == 0 || pickXIndex == screenPoints.length - 1) {
+			return defaultReturn;
 		}
 		var bestCandidateIndex = -1;
 		var minDistance = 10000000;
@@ -169,64 +258,97 @@ DataViz.DataRenderer.prototype = {
 			if (i < 0 || i > screenPointLength - 1) {
 				continue;
 			}
-			var dist = Math.sqrt((pos.x - this._screenPoints[i].x) * (pos.x - this._screenPoints[i].x) + (pos.y - this._screenPoints[i].y) * (pos.y - this._screenPoints[i].y));
+			var dist = Math.sqrt((pos.x - screenPoints[i].x) * (pos.x - screenPoints[i].x) + (pos.y - screenPoints[i].y) * (pos.y - screenPoints[i].y));
 			if (dist < minDistance) {
 				minDistance = dist;
 				bestCandidateIndex = i;
 			}
 		}
-		//good enough
+		//good enough ?
 		var delta = 8 / this._horizontalScale;
 		if (delta > 30) {
 			delta = 30;
 		}
 		if (minDistance <= delta) {
-			return bestCandidateIndex;
+			return { index: bestCandidateIndex, dist: minDistance };
 		}
-		return -1;
+		return defaultReturn;
 	},
 
-	_drawCircle: function(center) {
+	_getPickPoint: function(pos) {
+		var minDistance = 10000000;
+		var lineKey = '';
+		var index = -1;
+		for (var key in this._screenPoints) {
+			var result = this._getSingleLinePickPoint(pos, this._screenPoints[key]);
+			if (result.index == -1) {
+				continue;
+			}
+			if (result.dist < minDistance) {
+				minDistance = result.dist;
+				lineKey = key;
+				index = result.index;
+			}
+		}
+		if (index > -1) {
+			return {key: lineKey, index: index};
+		}
+		return {index: -1};
+	},
+
+	_drawCircle: function(lineKey, center) {
+		$('#metricsCanvas').css('cursor', 'pointer');
 		var ctx = this._context;
 		ctx.beginPath();
 		ctx.globalCompositeOperation="destination-over";
 		ctx.arc(center.x, center.y, 8, 0, 2 * Math.PI, false);
 		ctx.lineWidth = 4;
-        ctx.strokeStyle = 'green';
+		var color = this._lineColors[lineKey];
+        ctx.strokeStyle = this._convertAlphaColor(color, 0.5);
         ctx.stroke();
 		ctx.fill();
 	},
 
 	_clearCircle: function(index) {
+		$('#metricsCanvas').css('cursor', 'default')
 		var ctx = this._context;
-		
 		//restore line
-		var length = this._screenPoints.length;
+		var screenPoints = this._screenPoints[this._pickedLine];
+		var length = screenPoints.length;
 		var minIndex = (index - 10) < 0 ? 0 : index - 10;
 		var maxIndex = (index + 10) > length - 1 ? length - 1 : index + 10;
 
 		var startX = minIndex * this._horizontalScale;
 		var endX = (maxIndex - minIndex) * this._horizontalScale;
 
-		ctx.clearRect(minIndex * this._horizontalScale, 0, (maxIndex - minIndex) * this._horizontalScale, this._height);
+		ctx.clearRect(minIndex * this._horizontalScale, 0, (maxIndex - minIndex) * this._horizontalScale, this._height + 10);
 		
-		ctx.beginPath();
-		ctx.strokeStyle = 'red';
-		ctx.lineWidth = 1;
+		for (var key in this._screenPoints) {
+			var points = this._screenPoints[key];
+			var color = this._lineColors[key];
+			ctx.beginPath();
+			ctx.strokeStyle = this._convertColor(color);
+			ctx.lineWidth = 1;
 
-		for (var i = minIndex; i <= maxIndex; i++) {
-			var pos = this._screenPoints[i];
-			if (i == minIndex) {
-				ctx.moveTo(pos.x, pos.y);
-			} else {
-				ctx.lineTo(pos.x, pos.y);
+			for (var i = minIndex; i <= maxIndex; i++) {
+				var pos = points[i];
+				if (!pos) {
+					continue;
+				}
+				if (i == minIndex) {
+					ctx.moveTo(pos.x, pos.y);
+				} else {
+					ctx.lineTo(pos.x, pos.y);
+				}
 			}
+			ctx.stroke();
 		}
-		ctx.stroke();
 	},
 
 	pick: function(pos) {
-		var pickedIndex = this._getPickPoint(pos);
+		var result = this._getPickPoint(pos);
+		var pickedIndex = result.index;
+		var lineKey = result.key;
 		if (pickedIndex == -1) {
 			if (this._pickedIndex) {
 				this._clearCircle(this._pickedIndex);
@@ -241,9 +363,10 @@ DataViz.DataRenderer.prototype = {
 			this._clearCircle(this._pickedIndex);
 		}
 		this._pickedIndex = pickedIndex;
-		var point = this._screenPoints[pickedIndex];
+		this._pickedLine = lineKey;
+		var point = this._screenPoints[lineKey][pickedIndex];
 		//draw circle
-		this._drawCircle(point);
+		this._drawCircle(lineKey, point);
 	},
 
 	render: function(data, duration) {
@@ -253,17 +376,19 @@ DataViz.DataRenderer.prototype = {
 			canvas.width = width;
 			this._context = canvas.getContext('2d');
 			this._width = canvas.width;
-			this._height = canvas.height - 10;
+			this._height = canvas.height - 20;
 		}
 		this._context.globalCompositeOperation = 'source-over';
 		this.clear();
-		this._process(data, duration);
-		this._drawDimensions(data, duration);
+		this._duration = duration;
+		this._process(data);
+		this._drawDimensions(data);
+		this._showAxis();
 	},
 
 	clear: function() {
 		if (this._context) {
-			this._context.clearRect(0, 0, this._width, this._height);
+			this._context.clearRect(0, 0, this._width, this._height + 20);
 		}
 	}
 }
